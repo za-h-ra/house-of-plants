cee5cd506804921274834807013cfa5ced6a5edb
zahrakhan <zahra.khan1@icloud.com>
2020-08-11 15:28:22 -0400

    add token to user controller file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,13 @@ class UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
-
+    
     if @user.save
-      render json: @user, status: :created, location: @user
+      @token = encode({id: @user.id})
+      render json: {
+        user: @user.attributes.except("password"),
+        token: @token
+        }, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
