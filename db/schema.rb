e55790e23376913c3c8dc06ecf4c39a88c3e712f
zahrakhan <zahra.khan1@icloud.com>
2020-08-11 18:15:50 -0400

    add image url to plants schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_213020) do
+ActiveRecord::Schema.define(version: 2020_08_11_221441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_08_11_213020) do
     t.string "food_frequency"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_url"
   end
 
   create_table "users", force: :cascade do |t|
