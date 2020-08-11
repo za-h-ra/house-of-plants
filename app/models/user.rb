368b4d88e69138064c58151c139f8e45fda76ba1
zahrakhan <zahra.khan1@icloud.com>
2020-08-11 15:18:06 -0400

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password 
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { minimum: 6 }
+  
 end
