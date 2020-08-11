a1dc51b9712855d4265ce1c9d4dd0c397456a9e9
zahrakhan <zahra.khan1@icloud.com>
2020-08-11 15:24:29 -0400

    add encode and decode method to controller file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
+
+  def encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SECRET_KEY)
+  end
+
+  def decode(token)
+    decoded = JWT.decode(token, SECRET_KEY)[0]
+    HashWithIndifferentAccess.new decoded
+  end
 end
