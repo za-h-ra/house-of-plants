694d85179ba72fc27f0aeb1708f4a23a6ab8e07c
zahrakhan <zahra.khan1@icloud.com>
2020-08-13 16:33:41 -0400

    add categories file

---
+++ client/src/services/plant-categories.js
@@ -0,0 +1,26 @@
+import api from './api-helper'
+
+export const readAllPlantCategories = async () => {
+  const resp = await api.get('/plant_categories')
+  return resp.data
+}
+
+export const readOnePlantCategory = async (id) => {
+  const resp = await api.get(`/plant_categories/${id}`)
+  return resp.data
+}
+
+export const postPlantCategory = async (data) => {
+  const resp = await api.post('/plant_categories', { category: data })
+  return resp.data
+}
+
+export const putPlantCategory = async (id, data) => {
+  const resp = await api.put(`/plant_categories/${id}`, { category: data })
+  return resp.data
+}
+
+export const destoryPlantCategories = async (id) => {
+  const resp = await api.delete(`/plant_categories/${id}`)
+  return resp 
+}
