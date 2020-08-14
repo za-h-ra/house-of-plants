570e612117cd35f18e1f331b6cf20b8f70db0dc6
zahrakhan <zahra.khan1@icloud.com>
2020-08-14 17:13:24 -0400

    allow user to stay logged in

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/shared/Header/Header'
 import Main from './components/Main/Main'
+import { verifyUser } from './services/auth';
 
 
 function App() {
 
   const [currentUser, setCurrentUser] = useState(null)
 
+  useEffect(() => {
+    handleVerify()
+  }, [])
+  
+  const handleVerify = async () => {
+    const userData = await verifyUser()
+    setCurrentUser(userData)
+  }
 
   return (
     <div className="App">
--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3000'
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://house-of-plants.herokuapp.com/' : 'http://localhost:3000' 
 
 const api = axios.create({
   baseURL: baseUrl
