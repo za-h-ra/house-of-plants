9d4823115f46f643b60325d455fb128d4e7800b6
zahrakhan <zahra.khan1@icloud.com>
2020-08-14 12:15:42 -0400

    implement signin page

--- client/src/screens/SignIn.jsx
+++ client/src/screens/SignIn.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function SignIn(props) {
+  const [login, setLogin] = useState({
+    username: "",
+    password: "",
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setLogin({
+      ...login,
+      [name]: value
+    })
+  }
   
   return (
     <div>
       <form>
         <h2>sign in!</h2>
-        <input />
-        <input />
+        <input type="text" name="username" value={login.username}  />
+        <input type="password" name="password" value={login.password} />
         <Link>sign up</Link>
         <button>submit</button>
       </form>
