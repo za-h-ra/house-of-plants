ccd47c136f2b3f5ad0d888674df3f53577863b2b
zahrakhan <zahra.khan1@icloud.com>
2020-08-15 13:52:02 -0400

    create and style nav menu

--- client/src/components/shared/Header/Header.jsx
+++ client/src/components/shared/Header/Header.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
-import { Link, useHistory } from 'react-router-dom'
+import { Link, useHistory, NavLink } from 'react-router-dom'
 import { removeToken } from '../../../services/auth'
 import PlantProgress from '../../PlantProgress/PlantProgress'
 import Logo from '../Logo/Logo'
+import styled from 'styled-components'
+
+const NavBar = styled.nav`
+  background-color: #0a3d33;
+  width: 100%;
+  height: 90px;
+  text-decoration: none;
+  font-family: 'Quicksand', sans-serif;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 10px;
+`
+
+const LinksContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+  a {
+    align-items: center;
+    text-decoration: none;
+    color: #ffffff;
+    font-size: 15px;
+    height: 20px;
+    margin: 10px;
+    text-align: center;
+    &:hover, &.active {
+      text-shadow: 0 0 .75px #ffd9da, 0 0 .75px #ffd9da;
+    }
+  }
+`
 
 export default function Header(props) {
 	const history = useHistory()
@@ -15,7 +46,7 @@ export default function Header(props) {
 	}
 
 	return (
-		<header>
+		<NavBar>
 			<Logo/>
 
 			{props.currentUser ? (
@@ -24,12 +55,16 @@ export default function Header(props) {
 					<button onClick={handleLogout}>logout</button>
 				</>
 			) : (
-				<>
-					<Link to='/signin'>Sign In</Link> <br />
-					<Link to='/signup'>Create an Account</Link>
+          <>
+            <LinksContainer>
+              <Link to="/about">about</Link>
+              <Link to="/discover">discover</Link>
+              <Link to='/signin'>sign in</Link>
+              <Link to='/signup'>create an account</Link>
+            </LinksContainer>
 				</>
         )}
-      <hr />
+     
       {
         props.currentUser && (
           <>
@@ -37,6 +72,6 @@ export default function Header(props) {
           </>
         )
       }
-		</header>
+		</NavBar>
 	)
 }
