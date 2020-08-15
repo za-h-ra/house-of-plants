7aea9283e460cd014ccfb105cc84111fe7c6612f
zahrakhan <zahra.khan1@icloud.com>
2020-08-15 14:10:29 -0400

    install fontawesome library

---
+++ client/src/components/shared/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import Logo from '../Logo/Logo'
+
+export default function Footer() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
