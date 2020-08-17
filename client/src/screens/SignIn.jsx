5b38ab85110fabdda910cace3e38959c3e1c3b27
zahrakhan <zahra.khan1@icloud.com>
2020-08-17 14:40:46 -0400

    styled sign in page

--- client/src/screens/SignIn.jsx
+++ client/src/screens/SignIn.jsx
@@ -6,40 +6,63 @@ import styled from 'styled-components'
 const FormContainer = styled.div`
 	display: flex;
 	flex-flow: column;
-  align-items: center;
-  background-image: url("https://images.unsplash.com/photo-1518531933037-91b2f5f229cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80");
+	align-items: center;
+	background-image: url('https://images.unsplash.com/photo-1518531933037-91b2f5f229cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80');
 `
 
 const FormBox = styled.form`
 	width: 300px;
 	padding: 40px;
 	top: 50%;
-  left: 50%;
-  color: white;
+	left: 50%;
+	color: white;
 	// transform: translate(-50%, -50%);
-  background: #78e08f;
-  background-image: linear-gradient(#73C8A9, #373B44);
-  text-align: center;
-  border-radius: 10px;
-  margin-top: 100px;
-  margin-bottom: 100px;
-  box-shadow: 10px 10px rgba(0, 0, 0, 0.2) ;
+	background: #78e08f;
+	background-image: linear-gradient(#73c8a9, #373b44);
+	text-align: center;
+	border-radius: 10px;
+	margin-top: 100px;
+	margin-bottom: 100px;
+	box-shadow: 10px 10px rgba(0, 0, 0, 0.2);
 `
 const StyledInput = styled.input`
-  background-color: transparent;
-  width: 100%;
-  background: none;
-  display: block;
-  margin: 20px auto;
-  text-align: center;
-  border: 2px solid #0a3d62;
-  padding: 14px 10px;
-  outline: none;
-  color: white;
-  border-radius: 24px;
+	background-color: transparent;
+	width: 100%;
+	background: none;
+	display: block;
+	margin: 20px auto;
+	text-align: center;
+	border: 2px solid #0a3d62;
+	padding: 14px 10px;
+	outline: none;
+	color: white;
+	border-radius: 24px;
+  transition: 0.25s;
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
 `
 
-
+const StyledButton = styled.button`
+  background-color: transparent;
+  font-weight: 700;
+	width: 100%;
+  display: flex;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+	margin: 20px auto;
+	text-align: center;
+	border: 2px solid #0a3d62;
+	padding: 14px 10px;
+	outline: none;
+	color: white;
+  border-radius: 24px;
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+  transition: filter 0.2s ease-out;
+  &:hover {
+    filter: saturate(50%);
+    background-color: #0a3d62;
+  }
+`
 
 export default function SignIn(props) {
 	const [loginData, setLoginData] = useState({
@@ -80,9 +103,8 @@ export default function SignIn(props) {
 					value={loginData.password}
 					onChange={handleChange}
 				/>{' '}
-				<br />
-				<Link to='/signup'>sign up!</Link>
-				<button>submit</button>
+        <StyledButton>login</StyledButton>
+        <Link to='/signup'>sign up!</Link>
 			</FormBox>
 		</FormContainer>
 	)
