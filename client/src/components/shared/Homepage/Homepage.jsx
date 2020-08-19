ebdeb44370441c34c687c1fb01e9dd8b8fccb88d
zahrakhan <zahra.khan1@icloud.com>
2020-08-19 06:56:17 -0400

    homepage button styling

--- client/src/components/shared/Homepage/Homepage.jsx
+++ client/src/components/shared/Homepage/Homepage.jsx
@@ -22,13 +22,36 @@ const TextBox = styled.div`
   align-items: center;
   z-index: 1;
   color: white;
+  font-size: 50px;
+  font-weight: 500;
 
+`
+
+const StyledButton = styled.button`
+  background-color: #0a3d62; 
+  font-weight: 500;
+  width: 100%;
+  text-align: center;
+  margin: 20px auto;
+  border: 2px solid #0a3d62;
+  padding: 24px 20px;
+  color: white;
+  outline: none;
+  border-radius: 40px;
+  text-transform: uppercase;
+  font-family: 'Quicksand', sans-serif;
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+  &:hover {
+    background-color: #3c6382;
+    border: none;
+  }
 `
 
 export default function Homepage() {
 	return (
 		<HomepageContainer>
       <TextBox>Keep your plants alive</TextBox>
+      <Link to='/signup'><StyledButton>get started</StyledButton></Link>
 		</HomepageContainer>
 	)
 }
